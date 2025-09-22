e590d4bfad7913b57e28b4f5e88cc48a48e59553
Pedro Paulo <pedropaulocarvalhofarias@gmail.com>
2025-09-22 16:46:07 -0300

    Atividade da Aula 03

--- Aula03/atividade03.js
+++ Aula03/atividade03.js
@@ -2,12 +2,12 @@
 // Pedro Paulo de Carvalho Farias
 // 2414290173
 
-const menu = {100:1.7, 101:2.3, 102:2.6, 103:2.4, 104:2.5, 105:1.0};
+const menu = { 100: 1.7, 101: 2.3, 102: 2.6, 103: 2.4, 104: 2.5, 105: 1.0 };
 const pedidos = [
-  {codigo:100, qtd:2},
-  {codigo:103, qtd:1},
-  {codigo:105, qtd:3},
-  {codigo:0, qtd:0} // 0 finaliza
+  { codigo: 100, qtd: 2 },
+  { codigo: 103, qtd: 1 },
+  { codigo: 105, qtd: 3 },
+  { codigo: 0, qtd: 0 }, 
 ];
 let total = 0;
 let i = 0;
